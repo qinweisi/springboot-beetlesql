ebc0b3d1e3f31e0015175df573ddb5aa36919cad
qinweisi <qinweisi@foxmail.com>
2019-11-08 15:53:14 +0800

    测试kafka推送

---
+++ src/main/java/cn/com/qws/job/Test.java
@@ -0,0 +1,34 @@
+package cn.com.qws.job;
+
+import cn.com.qws.service.kafka.KafkaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+
+import java.util.UUID;
+
+/**
+ * @Description TODO
+ * @Author qinweisi
+ * @Date 2019/8/22 14:12
+ **/
+@Component
+@EnableScheduling
+public class Test {
+
+        @Autowired
+        private KafkaService kafkaService;
+
+
+    /**
+     * 定时任务
+     */
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void send() {
+        String message = UUID.randomUUID().toString();
+        kafkaService.push("test",message);
+    }
+}
