ad263f52779ca6876ac9f1392de4e2b5f957b443
qinweisi <qinweisi@foxmail.com>
2019-11-08 15:11:06 +0800

    orderBy

--- src/main/java/cn/com/qws/service/base/ServiceUtil.java
+++ src/main/java/cn/com/qws/service/base/ServiceUtil.java
@@ -2,16 +2,18 @@ package cn.com.qws.service.base;
 
 import cn.com.qws.common.DataGridOptions;
 import cn.com.qws.common.PagerModel;
+import cn.com.qws.utils.StringUtils;
 import org.beetl.sql.core.engine.PageQuery;
 
 public class ServiceUtil {
 
     /**
      * 将分页查询转为dataTable结构
+     *
      * @param query
      * @return
      */
-    public static PagerModel queryToTable(PageQuery query){
+    public static PagerModel queryToTable(PageQuery query) {
 
         PagerModel tb = new PagerModel();
         tb.setRows(query.getList());
@@ -21,14 +23,18 @@ public class ServiceUtil {
 
     /**
      * 将dataTable传过来的参数转为分页查询
+     *
      * @param opts
      * @return
      */
-    public static PageQuery pageToQuery(DataGridOptions opts){
+    public static PageQuery pageToQuery(DataGridOptions opts) {
         PageQuery query = new PageQuery();
         query.setPageSize(opts.getLength());
         query.setPageNumber(opts.getPageNumber());
         query.setParas(opts.getParams());
+        if (!StringUtils.isEmpty(opts.getOrder())) {
+            query.setOrderBy(opts.getOrder());
+        }
         return query;
     }
 }
